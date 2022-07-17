c9f14ea70cbe245dae4e092ff6290396c997df31
indranil2501 <indranil2501@gmail.com>
2022-07-17 16:18:53 +0530

    Line chart changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import LineChart from './LineChart';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <LineChart />
+      </header>
+    </div>
+  );
+}
+
+export default App;
