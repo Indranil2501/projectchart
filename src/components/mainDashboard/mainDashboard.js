09338653422506a925a8745899b09ef6746de828
indranil2501 <indranil2501@gmail.com>
2022-07-23 23:08:41 +0530

    Created a function to generate data

--- src/components/mainDashboard/mainDashboard.js
+++ src/components/mainDashboard/mainDashboard.js
@@ -3,6 +3,22 @@ import LineChart from '../LineChartComponent/LineChart';
 import './LineChart.css';
 
 const MainDashboard = () => {
+	useEffect(() => {
+		regenerateData();
+	  }, []);
+	
+	function regenerateData() {
+		const chartData = [];
+		for (let i = 0; i < 20; i++) {
+			const value = Math.floor(Math.random() * i + 3);
+			chartData.push({
+			label: i,
+			value,
+			tooltipContent: `<b>x: </b>${i}<br><b>y: </b>${value}`
+			});
+		}
+		setData(chartData);
+	}
 
 	return (
 		<>
